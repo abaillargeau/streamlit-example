79113fc39b6b93299ebfbf8453891fe5bceebed0
abaillargeau <144251327+abaillargeau@users.noreply.github.com>
2023-12-01 12:10:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import altair as alt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import datetime
 
 st.title("Steps visualization")
