3a019649836fe198f4886b5ccad4762a5bb5c607
abaillargeau <144251327+abaillargeau@users.noreply.github.com>
2023-12-01 12:08:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,36 @@
+import streamlit as st 
 import altair as alt
 import numpy as np
 import pandas as pd
-import streamlit as st
 import matplotlib.pyplot as plt
-"""
-# Analysis steps
+import datetime
 
-Let's try!
+st.title("Steps visualization")
 
-"""
-# Bouton
-if st.button("Push!"):
-    st.success(f"You pushed: {'Good boy!'}")
+# 1 # Selección del intervalo
 
-# Select date
-start_date = st.date_input("Select beginning day and time")
-end_date = st.date_input("Select end day and time")
+st.write('<p style="font-size:25px;">1. Interval definition', unsafe_allow_html=True)
+st.write('<p style="font-size:18px;">Define the range in which you want to view the data </p>', unsafe_allow_html=True)
 
+start_date = st.date_input("Select the beginning date")
+start_time = st.time_input("Select the beginning time")
+
+end_date = st.date_input("Select the ending date")
+end_time = st.time_input("Select the ending time")
+
+st.markdown(f"The results will be shown for the interval from <span style='color:blue;font-weight:bold;'>{start_date}</span> <span style='color:blue;font-weight:bold;'>{start_time}</span> to <span style='color:blue;font-weight:bold;'>{end_date}</span> <span style='color:blue;font-weight:bold;'>{end_time}</span>.", unsafe_allow_html=True)
+
+# 2 # Selección del pie
+
+st.write('<p style="font-size:25px;">2. Foot selection', unsafe_allow_html=True)
+
+feet_options = ['Right foot', 'Left foot']
+st.write('<p style="font-size:18px;">Choose the foot you want to visualize the data from.</p>', unsafe_allow_html=True)
+selection = st.selectbox("Select the foot",feet_options)
+
+if selection == 'Right foot':
+    st.markdown("The results will be shown for the <span style='color:blue;font-weight:bold;'>right foot</span>.", unsafe_allow_html=True)
+    
+elif selection == 'Left foot':
+    st.markdown("The results will be shown for the <span style='color:blue;font-weight:bold;'>left foot</span>.", unsafe_allow_html=True)
 
